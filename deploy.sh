f175e6ff4ed2cb0e6b275418048a3e3b45e39ae3
William Jia <wjia@WJia-mac-3.ott.signiant.com>
2019-03-29 15:04:18 -0400

    add healthcheck to the failover template

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 
+deploy_bucket=$1
+profile=$2
+region=$3
+parameters=$4
+
 rm -rf package
 
 mkdir package
@@ -12,14 +17,17 @@ cd package
     pip install -r ../requirements.txt --target .
 cd ..
 
-# Package up lambda code
+# Package up lambda code example:
+# --s3-bucket dev2-useast1-lambda-deploy
+# --profile dev2 --region us-east-1
+
 echo "Packaging up lambda for deployment..."
 aws cloudformation package \
     --template-file template.yaml \
-    --s3-bucket dev2-useast1-lambda-deploy \
+    --s3-bucket $deploy_bucket \
     --s3-prefix lambda-healthcheck-editor \
     --output-template-file packaged-template.yaml \
-    --profile dev2
+    --profile $profile --region $region
 RETCODE=$?
 if [ $RETCODE -ne 0 ]; then
     echo "Failed to create package"
@@ -27,8 +35,9 @@ if [ $RETCODE -ne 0 ]; then
 fi
 
 # Deploy the lambda
+# --parameter-overrides Region=us-east-1 DomainName=https://example.com
 echo "Deploying lambda..."
-aws cloudformation deploy     --capabilities CAPABILITY_IAM  --template-file ./packaged-template.yaml  --stack-name route53-disable-healthcheck-on-failover    --parameter-overrides Region=us-east-1 --profile dev2  --region us-east-1
+aws cloudformation deploy --capabilities CAPABILITY_IAM  --template-file ./packaged-template.yaml  --stack-name route53-disable-healthcheck-on-failover  --parameter-overrides $parameters --profile $profile  --region $region
 RETCODE=$?
 if [ $RETCODE -ne 0 ]; then
     echo "Failed to deploy lambda package"
